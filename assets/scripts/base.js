9c98611e18da4a91bd0864e1b5aec30d006c09ce
Laurent <laurent.touchet@gmail.com>
2025-04-30 12:43:11 +0200

    fix: animation menu

--- assets/scripts/base.js
+++ assets/scripts/base.js
@@ -1,3 +1,10 @@
 import "./components/Navigation.js";
 import './components/Header';
 import './components/Poster';
+
+/*!
+* preload class
+*/
+window.onload = function(e){
+    document.body.classList.remove ('preload');
+}
