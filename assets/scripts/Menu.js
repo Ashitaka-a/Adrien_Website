9dab05d8a5ce1821af0f6df0d5f1938228bb48ad
Ashitaka-a <adrien-simon@outlook.com>
2025-04-30 12:29:17 +0200

    header en JS

---
+++ assets/scripts/Menu.js
@@ -0,0 +1,23 @@
+export let links = [
+    {
+        label : 'Builds',
+        url : 'builds.html',
+    },
+    {
+        label : 'TierLists',
+        url : 'tierlists.html',
+    },
+    {
+        label : 'Patch Note',
+        url : 'patchnote.html',
+    },
+    {
+        label : 'Encyclopedia',
+        url : 'encyclopedia.html',
+    },
+    {
+        label : 'Login',
+        url : 'login.html',
+
+    },
+]   
