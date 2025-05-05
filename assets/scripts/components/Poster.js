0bfa3a96f938e7d2ec299abb7e6e38b690ad34c5
Laurent <laurent.touchet@gmail.com>
2025-05-05 15:33:27 +0200

    test laurent

--- assets/scripts/components/Poster.js
+++ assets/scripts/components/Poster.js
@@ -11,12 +11,12 @@ function handleParallaxEffect(selector, speedFactor) {
 function onScroll() {
   if (!ticking) {
     window.requestAnimationFrame(() => {
-      handleParallaxEffect(".poster__pictureartist", 0.2);
+      // handleParallaxEffect(".poster__pictureartist", 0.2);
       handleParallaxEffect(".poster__picture", 0.6);
-      handleParallaxEffect(".poster__pictureforms1", 0.4);
-      handleParallaxEffect(".poster__pictureforms2", 0.3);
-      handleParallaxEffect(".poster__pictureforms3", 0.2);
-      handleParallaxEffect(".poster__pictureforms4", 0.6);
+      // handleParallaxEffect(".poster__pictureforms1", 0.4);
+      // handleParallaxEffect(".poster__pictureforms2", 0.3);
+      // handleParallaxEffect(".poster__pictureforms3", 0.2);
+      // handleParallaxEffect(".poster__pictureforms4", 0.6);
       ticking = false;
     });
     ticking = true;
