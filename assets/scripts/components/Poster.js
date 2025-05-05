625959e3754abeeb927d923a59e605b6a82a608d
Adrien <adrien-simon@outlook.com>
2025-05-05 16:05:10 +0200

    test

--- assets/scripts/components/Poster.js
+++ assets/scripts/components/Poster.js
@@ -1,30 +1,8 @@
-let currentScroll = 0;
-let targetScroll = 0;
-let rafId;
-
-function animateParallax() {
-  currentScroll += (targetScroll - currentScroll) * 0.1;
-  currentScroll = parseFloat(currentScroll.toFixed(1));
-
-  handleParallaxEffect(".poster__pictureartist", 0.2, currentScroll);
-  handleParallaxEffect(".poster__picture", 0.4, currentScroll);
-  handleParallaxEffect(".poster__pictureforms1", 0.4, currentScroll);
-  handleParallaxEffect(".poster__pictureforms2", 0.3, currentScroll);
-  handleParallaxEffect(".poster__pictureforms3", 0.2, currentScroll);
-  handleParallaxEffect(".poster__pictureforms4", 0.4, currentScroll);
-
-  if (Math.abs(currentScroll - targetScroll) > 0.5) {
-    rafId = requestAnimationFrame(animateParallax);
-  } else {
-    currentScroll = targetScroll;
-    rafId = null;
-  }
-}
-
-function handleParallaxEffect(selector, speedFactor, scrollY) {
+function handleParallaxEffect(selector, speedFactor) {
+  const scrollY = window.scrollY;
   const parallaxElement = document.querySelector(selector);
   if (parallaxElement) {
-    parallaxElement.style.transform = `translate3d(0, ${scrollY * speedFactor}px, 0)`;
+    parallaxElement.style.transform = `translateY(${scrollY * speedFactor}px)`;
   }
 }
 
