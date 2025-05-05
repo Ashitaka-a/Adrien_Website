ca61e449555b24b4b78fb8d3662eb03041de5f2e
Adrien <adrien-simon@outlook.com>
2025-05-05 16:01:29 +0200

    réduction des images

--- assets/scripts/components/Poster.js
+++ assets/scripts/components/Poster.js
@@ -7,7 +7,6 @@ function handleParallaxEffect(selector, speedFactor) {
 }
 
 document.addEventListener("scroll", () => {
-  handleParallaxEffect(".poster__pictureartist", 0.2);
   handleParallaxEffect(".poster__picture", 0.6);
   handleParallaxEffect(".poster__pictureforms1", 0.4);
   handleParallaxEffect(".poster__pictureforms2", 0.3);
