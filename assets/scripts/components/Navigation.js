14f8c640b185d4fa0176b14bc4ca41011177ab15
Adrien <adrien-simon@outlook.com>
2025-05-06 23:28:06 +0200

    burger menu / new

--- assets/scripts/components/Navigation.js
+++ assets/scripts/components/Navigation.js
@@ -7,5 +7,6 @@ links.forEach(function(link) {
     menu += `<li class="navbar__menu__item"><a href="${link.url}" class="navbar__menu__link">${link.label}</a></li>`;
 });
 
+menu += `<li class="navbar__menu__item bottom-image"><img src="pictures/header_open_menu/character_header.webp" alt="bottom image"></li>`;
 menu += `</ul>`;
 menuContainer.innerHTML = menu;
