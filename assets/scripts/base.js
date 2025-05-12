b6bf8a818983139645a37cda73b23ab262c17f93
Adrien <adrien-simon@outlook.com>
2025-05-12 14:53:59 +0200

    extensions

--- assets/scripts/base.js
+++ assets/scripts/base.js
@@ -1,13 +1,15 @@
 import { mainNavigation } from './components/Navigation';
 import './components/Poster';
-import './components/Eidolons';
 
 window.addEventListener('DOMContentLoaded',() => {
     document.body.classList.remove ('preload');
     const menu = new mainNavigation;
 
     menu.createMenu();
-})
+});
+
+import './components/Eidolons';
+
 
 /*!
 * preload class
